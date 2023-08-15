4b9d1b78a46162a208a538d674f884b10d380d91
AllanMurara <allan@logicmindz.com.br>
2023-08-15 16:43:29 -0300

    implement graphql and schema

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -3,16 +3,33 @@
  * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
  * For more information, see https://remix.run/file-conventions/entry.client
  */
-
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { RemixBrowser } from "@remix-run/react";
+
+
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
+
+
+
+function Client() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
+    uri: "https://play-test.api.footium.club/api/graphql", // the same uri in our entry.server file
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <RemixBrowser />
+    </ApolloProvider>
+  );
+}
 
 startTransition(() => {
   hydrateRoot(
     document,
     <StrictMode>
-      <RemixBrowser />
+      <Client />
     </StrictMode>
   );
 });
